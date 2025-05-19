9944b2be4b27d5268757cdb70dc03cae93c007d0
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-05-19 20:32:38 -0300

    feat: Inicializando projeto via Spring Initializr

---
+++ src/test/java/com/br/anampet/AnampetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.anampet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnampetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

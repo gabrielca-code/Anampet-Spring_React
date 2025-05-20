3e52e7bb5a2e71c511a65e10f8b0425648eb35e4
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-05-19 21:11:24 -0300

    feat: criando classe de domínio usuário

--- src/main/java/com/br/anampet/domain/usuario/Usuario.java
+++ src/main/java/com/br/anampet/domain/usuario/Usuario.java
@@ -1,4 +1,28 @@
 package com.br.anampet.domain.usuario;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.service.annotation.GetExchange;
+
+@Entity(name = "usuario")
+@Table(name = "usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String crmv;
+    private String email;
+    private String login;
+    private String cpf;
+    private String senha;
+
 }

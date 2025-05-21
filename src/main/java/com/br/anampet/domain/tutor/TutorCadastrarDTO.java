1309aa1f7c8cad811b1f27e22999727fb44a5564
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-05-21 20:55:42 -0300

    feat: Criando DTO para cadastro de tutor

---
+++ src/main/java/com/br/anampet/domain/tutor/TutorCadastrarDTO.java
@@ -0,0 +1,21 @@
+package com.br.anampet.domain.tutor;
+
+import com.br.anampet.domain.usuario.Usuario;
+
+import java.util.Date;
+
+public record TutorCadastrarDTO(
+        String nome,
+        String telefone,
+        String email,
+        Date data_nascimento,
+        String estado,
+        String cidade,
+        String bairro,
+        String rua,
+        String numero,
+        String complemento,
+        String observacao,
+        Usuario usuario
+) {
+}

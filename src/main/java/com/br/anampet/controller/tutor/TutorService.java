326ee974fc805d7970da8d13bd2c913a6dccab66
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-05-26 22:40:27 -0300

    feat: Camada service de tutor criada

---
+++ src/main/java/com/br/anampet/controller/tutor/TutorService.java
@@ -0,0 +1,40 @@
+package com.br.anampet.controller.tutor;
+
+import com.br.anampet.domain.tutor.Tutor;
+import com.br.anampet.domain.tutor.TutorCadastrarDTO;
+import com.br.anampet.domain.tutor.TutorEditarDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TutorService {
+
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    public List<Tutor> obterTutores() {
+        return tutorRepository.findAll();
+    }
+
+    public Tutor obterTutor(Long id) {
+        var tutor = tutorRepository.findById(id);
+        return tutor.get();
+    }
+
+    public Tutor criarTutor(TutorCadastrarDTO tutorDto) {
+        return tutorRepository.save(new Tutor(tutorDto));
+    }
+
+    public Tutor editarTutor(TutorEditarDTO tutorDto) {
+        var tutor = tutorRepository.getReferenceById(tutorDto.id());
+        tutor.editarCampos(tutorDto);
+        return tutor;
+    }
+
+    public void excluirTUtor(Long id) {
+        tutorRepository.deleteById(id);
+    }
+
+}

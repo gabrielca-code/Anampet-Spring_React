db533d88300ee243905b6787cb53654a528ebd79
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-05-27 22:19:30 -0300

    fix: Erro no bean

--- src/main/java/com/br/anampet/infra/NotFoundException.java
+++ src/main/java/com/br/anampet/infra/NotFoundException.java
@@ -1,8 +1,5 @@
 package com.br.anampet.infra;
 
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(Object id) {

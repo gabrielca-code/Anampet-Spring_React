f64806e45e081872d5320d0716b10c7d5f9636d2
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-05-27 22:19:54 -0300

    feat: Ajustado retorno do DTO de usuário no listar tutor

--- src/main/java/com/br/anampet/domain/tutor/TutorListarDTO.java
+++ src/main/java/com/br/anampet/domain/tutor/TutorListarDTO.java
@@ -1,6 +1,7 @@
 package com.br.anampet.domain.tutor;
 
 import com.br.anampet.domain.usuario.Usuario;
+import com.br.anampet.domain.usuario.UsuarioListarDTO;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,11 +22,11 @@ public record TutorListarDTO(
     String numero,
     String complemento,
     String observacao,
-    Usuario usuario
+    UsuarioListarDTO usuario
 ) {
 
     public TutorListarDTO(Tutor tutor) {
-        this(tutor.getId(), tutor.getNome(), tutor.getTelefone(), tutor.getEmail(), tutor.getData_nascimento(), tutor.getEstado(), tutor.getCidade(), tutor.getBairro(), tutor.getRua(), tutor.getNumero(), tutor.getComplemento(), tutor.getObservacao(), tutor.getUsuario());
+        this(tutor.getId(), tutor.getNome(), tutor.getTelefone(), tutor.getEmail(), tutor.getData_nascimento(), tutor.getEstado(), tutor.getCidade(), tutor.getBairro(), tutor.getRua(), tutor.getNumero(), tutor.getComplemento(), tutor.getObservacao(), new UsuarioListarDTO(tutor.getUsuario()));
     }
 
 }

d79c60928f63144e8b2fc69798f7d1d3b7267071
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-05-28 07:45:20 -0300

    feat: Tratando MethodArgumentNotValidException

--- src/main/java/com/br/anampet/infra/ResourceHandlerException.java
+++ src/main/java/com/br/anampet/infra/ResourceHandlerException.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,6 +23,14 @@ public class ResourceHandlerException extends RuntimeException {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<StandardError> errosDados(DataIntegrityViolationException ex, HttpServletRequest request) {
+        String error = "Erro na validação dos dados!";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> errosDados(MethodArgumentNotValidException ex, HttpServletRequest request) {
         String error = "Erro na validação dos dados!";
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
